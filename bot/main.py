b2a3f7329b6be912a7e5c918aee87f52ea7eb5eb
Jasraj Johal <44610634+roosevelt47@users.noreply.github.com>
2021-02-14 15:53:57 +0530

    added auto-response v1

--- bot/main.py
+++ bot/main.py
@@ -12,5 +12,31 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send("pong")
 
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+    return
+    message_str = message.content.lower()
+    message_list = message_str.split()
+    if "hello" in message.content.lower():
+        await message.channel.send("Hey")
+    elif "yo" in message.content.lower():
+        await message.channel.send("yo")
+    elif "gm" in message.content.lower():
+        await message.channel.send("Good Morning")
+    elif "gn" in message.content.lower():
+        await message.channel.send("Good Night")
+    elif "good morning" in message.content.lower():
+        await message.channel.send("Good Morning !")
+    elif "good night" in message.content.lower():
+        await message.channel.send("Good Night !")
+    elif "hey" in message.content.lower():
+        await message.channel.send("Hello !")
+    elif "sup" in message.content.lower():
+        await message.channel.send("Sup")
+    elif "hi" in message_list:
+        await message.channel.send("Hello!")
+
+
 if __name__ == "__main__":
     bot.run(TOKEN)
