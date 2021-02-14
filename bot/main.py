65d9522c12eee6b88c3f742dac71198027d5b0e8
Jasraj Johal <44610634+roosevelt47@users.noreply.github.com>
2021-02-14 16:55:12 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,17 +1,18 @@
 import os
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix=";")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
+    print(f"Logged in as {bot.user.name}({bot.user.id}) OKAYYYYYYYYYYYYYYYYY")
 
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
 
+'''
 @bot.event
 async def on_message(message):
     if message.author.bot:
@@ -36,6 +37,7 @@ async def on_message(message):
         await message.channel.send("Sup")
     elif "hi" in message_list:
         await message.channel.send("Hello!")
+'''
 
 @bot.command()
 async def gapply(messages, email):
